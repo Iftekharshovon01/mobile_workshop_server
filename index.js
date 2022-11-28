5dc7b77e9eeff778d0c98f616ef69fbe9a55b7e4
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-11-28 22:56:48 +0600

    products working

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { req, res } = require('express');
+const { req, res, query } = require('express');
 const express = require('express')
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors());
+app.use(express.json());
 
 
 
@@ -16,18 +17,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const categoriesCollectio = client.db('mobileWorkshop').collection('categories');
+        const categoriesCollection = client.db('mobileWorkshop').collection('categories');
         const productsCollection = client.db('mobileWorkshop').collection('products');
+        const usersCollection = client.db('mobileWorkshop').collection('users');
+        const bookingCollection = client.db('mobileWorkshop').collection('booking');
         app.get('/categories', async (req, res) => {
             const query = {};
-            const categories = await categoriesCollectio.find(query).toArray();
+            const categories = await categoriesCollection.find(query).toArray();
             res.send(categories);
         })
-        app.post('/products', async (req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
-            const products = await categoriesCollectio.find(query).toArray();
+            const products = await productsCollection.find(query).toArray();
             res.send(products);
         })
+        app.get('/products/:company', async (req, res) => {
+            const company = req.params.company;
+            const query = { company: company };
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+
+
     }
     finally { }
 }
